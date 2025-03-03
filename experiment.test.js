3828790395c319f69e67330a72ee4a03a14b010f
anilhelvaci <anilhelvaci96@gmail.com>
2025-03-03 09:55:59 +0300

    chore: start cleaning up

--- experiment.test.js
+++ experiment.test.js
@@ -335,6 +335,9 @@ test("attest", async (t) => {
   t.pass();
 });
 
+/**
+ * submit this vaa to tokenBridge and globalAccountant
+ */
 test("createVaa", async (t) => {
   const testTokenAddr = Wormhole.parseAddress(
       "Solana",
@@ -344,10 +347,7 @@ test("createVaa", async (t) => {
       "Solana",
       "ENG1wQ7CQKH8ibAJ1hSLmJgL9Ucg6DRDbj752ZAfidLA",
     ),
-    receiver = Wormhole.parseAddress(
-      "Wormchain",
-      "wormhole1cyyzpxplxdzkeea7kwsydadg87357qna3zg3tq",
-    ),
+    receiver = Wormhole.parseAddress("Wormchain", "ibc-translator"),
     base64EncodedReceiver = new TextEncoder().encode(
       "osmo1lwc58qfnwycw990cvq0yefnjqqvjgadlyaxdp6",
     ),
@@ -401,85 +401,9 @@ test("createVaa", async (t) => {
   t.pass();
 });
 
-test.only("create payload 3", async (t) => {
-  const testTokenAddr = Wormhole.parseAddress(
-      "Solana",
-      "2WDq7wSs9zYrpx2kbHDA4RUTRch2CCTP6ZWaH4GNfnQQ",
-    ),
-    emitterAddress = Wormhole.parseAddress(
-      "Solana",
-      "ENG1wQ7CQKH8ibAJ1hSLmJgL9Ucg6DRDbj752ZAfidLA",
-    ),
-    ibcTranslator = Wormhole.parseAddress(
-      "Wormchain",
-      "wormhole1wn625s4jcmvk0szpl85rj5azkfc6suyvf75q6vrddscjdphtve8sca0pvl",
-    ),
-    transferVaa = {
-      version: 1,
-      guardianSetIndex: 0,
-      signatures: [],
-      timestamp: 0,
-      nonce: 0,
-      emitterChain: 1,
-      emitterAddress: emitterAddress.toUniversalAddress().toString(),
-      sequence: BigInt(Math.floor(Math.random() * 100000000)),
-      consistencyLevel: 0,
-    },
-    amount = 1000000000, // Example amount
-    tokenAddr = testTokenAddr.toUniversalAddress().toUint8Array(), // Solana contract universal address
-    tokenChain = 1, // Solana chain id
-    recipient = ibcTranslator.toUniversalAddress().toUint8Array(), // Recipient addr is ibc translator
-    recipientChain = 3104, // Wormchain id
-    from = emitterAddress.toUniversalAddress().toUint8Array(), // External address
-    terraRecepient = Wormhole.parseAddress(
-      "Terra2",
-      "terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v",
-    ),
-    osmoRecipient = Wormhole.parseAddress(
-      "Osmosis",
-      "osmo1lwc58qfnwycw990cvq0yefnjqqvjgadlyaxdp6",
-    ),
-    base64EncodedReceiver = new TextEncoder().encode(
-      "osmo1lwc58qfnwycw990cvq0yefnjqqvjgadlyaxdp6",
-    ),
-    // Encode contract payload
-    contractPayloadObj = {
-      gateway_transfer: {
-        chain: 20,
-        fee: "0",
-        nonce: 77993,
-        recipient: Buffer.from(base64EncodedReceiver).toString("base64"),
-      },
-    },
-    contractPayloadString = JSON.stringify(contractPayloadObj),
-    contractPayload = new TextEncoder().encode(contractPayloadString),
-    payloadBytes = createPayload3(
-      amount,
-      tokenAddr,
-      tokenChain,
-      recipient,
-      recipientChain,
-      from,
-      contractPayload,
-    );
-  transferVaa.payload = {
-    type: "3",
-    hex: "000000000000000000000000000000000000000000000000000000003b9aca00165809739240a0ac03b98440fe8985548e3aa683cd0d4d9df5b5659669faa301000174f4aa42b2c6d967c041f9e83953a2b271a8708c4fa80d306d6c312686eb664f0c20c69a1b1a65dd336bf1df6a77afb501fc25db7fc0938cb08595a9ef473265cb4f7b22676174657761795f7472616e73666572223a7b22636861696e223a31382c22666565223a2230222c226e6f6e6365223a37373939322c22726563697069656e74223a7b2230223a35332c2231223a3131362c2232223a34382c2233223a3131362c2234223a3134392c2235223a3130382c2236223a3131332c2237223a382c2238223a302c2239223a3233322c223130223a34392c223131223a3135322c223132223a312c223133223a32382c223134223a3230332c223135223a3231322c223136223a3232312c223137223a3234312c223138223a38352c223139223a3130397d7d7d",
-  };
-
-  console.log(Buffer.from(payloadBytes).toString("hex")); // Print as hex string
-
-  transferVaa.signatures = sign(
-    ["cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0"],
-    transferVaa,
-  );
-
-  const serialized = Buffer.from(serialiseVAA(transferVaa), "hex");
-
-  console.log("VAA", serialized);
-  t.pass();
-});
-
+/**
+ * submit this vaa to ibcTranslator
+ */
 test("update channel info", async (t) => {
   const updateChannelVaa = {
     version: 1,
@@ -515,119 +439,9 @@ test("update channel info", async (t) => {
   t.pass();
 });
 
-test("test-vaa", async (t) => {
-  const testTokenAddr = Wormhole.parseAddress(
-      "Solana",
-      "2WDq7wSs9zYrpx2kbHDA4RUTRch2CCTP6ZWaH4GNfnQQ",
-    ),
-    emitterAddress = Wormhole.parseAddress(
-      "Solana",
-      "ENG1wQ7CQKH8ibAJ1hSLmJgL9Ucg6DRDbj752ZAfidLA",
-    ),
-    ibcTranslator = Wormhole.parseAddress(
-      "Wormchain",
-      "wormhole1wn625s4jcmvk0szpl85rj5azkfc6suyvf75q6vrddscjdphtve8sca0pvl",
-    ),
-    // Const terraRecipient = Wormhole.parseAddress("Terra2", "terra1x46rqay4d3cssq8gxxvqz8xt6nwlz4td20k38v");
-    osmoRecipient = Wormhole.parseAddress(
-      "Osmosis",
-      "osmo1lwc58qfnwycw990cvq0yefnjqqvjgadlyaxdp6",
-    ),
-    // Encode contract payload
-    contractPayloadObj = {
-      gateway_transfer: {
-        chain: 20,
-        fee: 0,
-        nonce: 7825,
-        recipient: Buffer.from(
-          osmoRecipient.toNative().toUint8Array(),
-        ).toString("base64"),
-      },
-    },
-    // Buffer.from(terraRecipient.toUint8Array()).toString('base64')
-    contractPayloadString = JSON.stringify(contractPayloadObj),
-    contractPayload = new TextEncoder().encode(contractPayloadString),
-    test = {
-      version: 1,
-      guardianSetIndex: 0,
-      signatures: [],
-      timestamp: 1,
-      nonce: 74182,
-      emitterChain: 1,
-      emitterAddress: emitterAddress.toUniversalAddress().toUint8Array(),
-      sequence: "274721",
-      consistencyLevel: 0,
-      payload: {
-        module: "TokenBridge",
-        type: "TransferWithPayload",
-        amount: 1000000000,
-        tokenAddress: testTokenAddr.toUniversalAddress().toUint8Array(),
-        tokenChain: 1,
-        toAddress: ibcTranslator.toUniversalAddress().toUint8Array(),
-        chain: 3104,
-        fromAddress: emitterAddress.toUniversalAddress().toUint8Array(),
-        payload: Buffer.from(contractPayload).toString("hex"),
-      },
-    };
-
-  //   Test.payload = paylaod3;
-
-  test.signatures = sign(
-    ["cfb12303a19cde580bb4dd771639b0d26bc68353645571a8cff516ab2ee113a0"],
-    test,
-  );
-
-  console.log(test);
-
-  const serialized = serialiseVAA(test);
-  console.log(serialized);
-  console.log("BASE_64:", Buffer.from(serialized, "hex").toString("base64"));
-  t.pass();
-});
-
-test("upgrade-vaa", async (t) => {
-  const testTokenAddr = Wormhole.parseAddress(
-      "Solana",
-      "2WDq7wSs9zYrpx2kbHDA4RUTRch2CCTP6ZWaH4GNfnQQ",
-    ),
-    emitterAddress = Wormhole.parseAddress(
-      "Solana",
-      "0000000000000000000000000000000000000000000000000000000000000004",
-    ),
-    receiver = Wormhole.parseAddress(
-      "Wormchain",
-      "wormhole1cyyzpxplxdzkeea7kwsydadg87357qna3zg3tq",
-    ),
-    base = createVAA("GatewayGovernance:ScheduleUpgrade", {
-      guardianSet: 0,
-      nonce: 0,
-      timestamp: 0,
-      signatures: [],
-      sequence: 0n,
-      emitterChain: "Solana",
-      emitterAddress: emitterAddress.toUniversalAddress(),
-      consistencyLevel: 0,
-      payload: {
-        // Protocol: 'GatewayGovernance',
-        // Action: 'ScheduleUpgrade',
-        chain: "Wormchain",
-        actionArgs: {
-          name: "v2.26.0",
-          height: 50,
-        },
-      },
-    }),
-    // Const rawVaa = createVAA("GatewayGovernance:ScheduleUpgrade", base);
-    serializedVaa = serialize(base),
-    vaa = deserialize("GatewayGovernance:ScheduleUpgrade", serializedVaa);
-
-  console.log(base);
-  console.log(serializedVaa);
-  console.log(vaa);
-  console.log(Buffer.from(serializedVaa).toString("hex"));
-  t.pass();
-});
-
+/**
+ * submit this vaa to wormchaind wormhole module
+ */
 test("mw-set-vaa", async (t) => {
   const emitterAddress = Wormhole.parseAddress(
       "Solana",
@@ -667,3 +481,10 @@ test("mw-set-vaa", async (t) => {
   console.log(Buffer.from(serializedVaa).toString("hex"));
   t.pass();
 });
+
+/**
+ * submit this vaa to globalAccountant
+ */
+test.todo(
+  "introduce tokenBridge as Wormchain emitter address in globalAccountant",
+);
