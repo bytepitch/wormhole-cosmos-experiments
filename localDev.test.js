fc92f9bea4d3726b60244ab124b3206f4502474d
anilhelvaci <anilhelvaci96@gmail.com>
2025-03-07 07:56:46 +0300

    chore: re-organize test order

--- localDev.test.js
+++ localDev.test.js
@@ -32,6 +32,10 @@ const config = {
     'Solana',
     '0000000000000000000000000000000000000000000000000000000000000004',
   ),
+  tokenBridgeAddress: Wormhole.parseAddress(
+    'Wormchain',
+    'wormhole1eyfccmjm6732k7wp4p6gdjwhxjwsvje44j0hfx8nkgrm8fs7vqfssvpdkx',
+  ),
   ibcTranslatorAddress: Wormhole.parseAddress(
     'Wormchain',
     'wormhole1wn625s4jcmvk0szpl85rj5azkfc6suyvf75q6vrddscjdphtve8sca0pvl',
@@ -50,16 +54,99 @@ test.before(async t => {
   };
 });
 
+// ////////////////// Bootstrap Environment //////////////////
+
+test('update-channel-info', async t => {
+  const { client } = t.context;
+  const { govEmitterAddress } = config;
+
+  /**
+   * @type {import('./vaaHelpers.js').EmitterInfo}
+   */
+  const emitterInfo = {
+    emitterChain: 'Solana',
+    emitterAddress: govEmitterAddress.toUniversalAddress(),
+  };
+
+  /**
+   * @type {import('./vaaHelpers.js').UpdateChannelPayloadInfo}
+   */
+  const payloadInfo = {
+    channelId: 'channel-0',
+    // This should be something present in https://github.com/wormhole-foundation/wormhole-sdk-ts/blob/80bbcd9ec0a8bbdc564c812996807b6cd98e0757/core/base/src/constants/chains.ts#L6
+    channelChain: 'Osmosis',
+  };
+
+  const vaa = createUpdateChannelVAA(emitterInfo, payloadInfo, true);
+  const tx = await sendUpdateChannelInfoTx(client, vaa);
+  console.log('TX:', tx);
+  t.pass();
+});
+
+test('mw-set-vaa', async t => {
+  const { client } = t.context;
+  const { ibcTranslatorAddress, govEmitterAddress } = config;
+
+  /**
+   * @type {import('./vaaHelpers.js').EmitterInfo}
+   */
+  const emitterInfo = {
+    emitterChain: 'Solana',
+    emitterAddress: govEmitterAddress.toUniversalAddress(),
+  };
+
+  /**
+   * @type {import('./vaaHelpers.js').SetIbcMwPayloadInfo}
+   */
+  const payloadInfo = {
+    chain: 'Wormchain',
+    contractAddress: ibcTranslatorAddress.toUniversalAddress(),
+  };
+
+  const vaa = createSetMwVAA(emitterInfo, payloadInfo, true);
+  t.log(Buffer.from(serialize(vaa)).toString('hex'));
+
+  const tx = await sendGatewayGovTx(client, vaa);
+  console.log('TX:', tx);
+
+  t.pass();
+});
+
+test('register-tb-to-accountant', async t => {
+  const { client } = t.context;
+  const { govEmitterAddress, tokenBridgeAddress } = config;
+
+  /**
+   * @type {import('./vaaHelpers.js').EmitterInfo}
+   */
+  const emitterInfo = {
+    emitterChain: 'Solana',
+    emitterAddress: govEmitterAddress.toUniversalAddress(),
+  };
+
+  const vaa = createRegisterWormchainToAccountantVAA(
+    emitterInfo,
+    tokenBridgeAddress.toUniversalAddress(),
+  );
+  t.log(Buffer.from(serialize(vaa)).toString('hex'));
+
+  const tx = await sendGaSubmitVaasTx(client, vaa);
+  console.log('TX:', tx);
+
+  t.pass();
+});
+
 test('attest', async t => {
   const { client } = t.context;
+  const { emitterAddress, tokenAddress } = config;
 
   const emitterInfo = {
-    emitterAddress: config.emitterAddress.toUniversalAddress(),
+    emitterAddress: emitterAddress.toUniversalAddress(),
     emitterChain: 'Solana',
   };
 
   const payloadInfo = {
-    tokenAddress: config.tokenAddress.toUniversalAddress(),
+    tokenAddress: tokenAddress.toUniversalAddress(),
     tokenChain: 'Solana',
     decimals: 6,
     symbol: 'SOLT',
@@ -75,9 +162,8 @@ test('attest', async t => {
   t.pass();
 });
 
-/**
- * submit this vaa to tokenBridge and globalAccountant
- */
+// ////////////////// Use Case //////////////////
+
 test('send-to-osmo', async t => {
   const { client } = t.context;
   const { emitterAddress, tokenAddress, ibcTranslatorAddress } = config;
@@ -110,7 +196,7 @@ test('send-to-osmo', async t => {
   const payloadInfo = {
     payloadId: 3,
     fee: 0,
-    tokenAmount: 10000000,
+    tokenAmount: 30000000,
     tokenAddress: tokenAddress.toUniversalAddress(),
     tokenChain: 'Solana',
     toAddress: ibcTranslatorAddress.toUniversalAddress(),
@@ -126,95 +212,7 @@ test('send-to-osmo', async t => {
   t.pass();
 });
 
-/**
- * submit this vaa to ibcTranslator
- */
-test('update-channel-info', async t => {
-  const { client } = t.context;
-  const { govEmitterAddress } = config;
-
-  /**
-   * @type {import('./vaaHelpers.js').EmitterInfo}
-   */
-  const emitterInfo = {
-    emitterChain: 'Solana',
-    emitterAddress: govEmitterAddress.toUniversalAddress(),
-  };
-
-  /**
-   * @type {import('./vaaHelpers.js').UpdateChannelPayloadInfo}
-   */
-  const payloadInfo = {
-    channelId: 'channel-0',
-    // This should be something present in https://github.com/wormhole-foundation/wormhole-sdk-ts/blob/80bbcd9ec0a8bbdc564c812996807b6cd98e0757/core/base/src/constants/chains.ts#L6
-    channelChain: 'Osmosis',
-  };
-
-  const vaa = createUpdateChannelVAA(emitterInfo, payloadInfo, true);
-  const tx = await sendUpdateChannelInfoTx(client, vaa);
-  console.log('TX:', tx);
-  t.pass();
-});
-
-/**
- * submit this vaa to wormchaind wormhole module
- */
-test('mw-set-vaa', async t => {
-  const { ibcTranslatorAddress, govEmitterAddress } = config;
-
-  /**
-   * @type {import('./vaaHelpers.js').EmitterInfo}
-   */
-  const emitterInfo = {
-    emitterChain: 'Solana',
-    emitterAddress: govEmitterAddress.toUniversalAddress(),
-  };
-
-  /**
-   * @type {import('./vaaHelpers.js').SetIbcMwPayloadInfo}
-   */
-  const payloadInfo = {
-    chain: 'Wormchain',
-    contractAddress: ibcTranslatorAddress.toUniversalAddress(),
-  };
-
-  const vaa = createSetMwVAA(emitterInfo, payloadInfo, true);
-  console.log(vaa);
-
-  const tx = await sendGatewayGovTx(vaa);
-  console.log('TX:', tx);
-
-  t.pass();
-});
-
-/**
- * submit this vaa to globalAccountant
- */
-test('register-tb-to-accountant', async t => {
-  const { client } = t.context;
-  const { govEmitterAddress, tokenBridgeAddress } = config;
-
-  /**
-   * @type {import('./vaaHelpers.js').EmitterInfo}
-   */
-  const emitterInfo = {
-    emitterChain: 'Solana',
-    emitterAddress: govEmitterAddress.toUniversalAddress(),
-  };
-
-  const vaa = createRegisterWormchainToAccountantVAA(
-    emitterInfo,
-    tokenBridgeAddress.toUniversalAddress(),
-  );
-  t.log(Buffer.from(serialize(vaa)).toString('hex'));
-
-  const tx = await sendGaSubmitVaasTx(client, vaa);
-  console.log('TX:', tx);
-
-  t.pass();
-});
-
-test('introduce transfer vaa to globalAccountant', async t => {
+test('introduce-transfer-vaa-to-globalAccountant', async t => {
   const { client } = t.context;
   const { emitterAddress, tokenAddress, ibcTranslatorAddress } = config;
 
@@ -245,7 +243,7 @@ test('introduce transfer vaa to globalAccountant', async t => {
    */
   const payloadInfo = {
     fee: 0,
-    tokenAmount: 10000000,
+    tokenAmount: 30000000,
     tokenAddress: tokenAddress.toUniversalAddress(),
     tokenChain: 'Solana',
     toAddress: ibcTranslatorAddress.toUniversalAddress(),
@@ -269,3 +267,7 @@ test('introduce transfer vaa to globalAccountant', async t => {
 
   t.pass();
 });
+
+test.todo('send from osmo');
+
+test.todo('verify-vaa');
